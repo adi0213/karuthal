b9ab9839233810455031810f0a4fef30380bf152
Adith S <adithsiju0213@gmail.com>
2024-09-07 18:16:52 +0530

    made changes

--- lib/signup.dart
+++ lib/signup.dart
@@ -2,6 +2,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:new_chilla/design.dart';
+import 'package:new_chilla/home.dart';
 
 class Signup extends StatefulWidget {
   const Signup({super.key});
@@ -12,6 +13,9 @@ class Signup extends StatefulWidget {
 
 class _SignupState extends State<Signup> {
   final _formKey = GlobalKey<FormState>();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   bool _isPasswordVisible = false;
 
   @override
@@ -24,7 +28,7 @@ class _SignupState extends State<Signup> {
             child: Column(
               children: [
                 Padding(
-                  padding: const EdgeInsets.all(16.0),
+                  padding: const EdgeInsets.all(20.0),
                   child: Form(
                     key: _formKey,
                     child: Column(
@@ -49,29 +53,58 @@ class _SignupState extends State<Signup> {
                         const SizedBox(height: 30),
                         _buildLabelText(context, "User Name"),
                         const SizedBox(height: 2),
-                        _buildProjectedTextFormField(isPassword: false),
+                        _buildProjectedTextFormField(
+                          controller: _usernameController,
+                          isPassword: false,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your username';
+                            }
+                            return null;
+                          },
+                        ),
                         const SizedBox(height: 20),
                         _buildLabelText(context, "Email"),
                         const SizedBox(height: 2),
-                        _buildProjectedTextFormField(isPassword: false),
+                        _buildProjectedTextFormField(
+                          controller: _emailController,
+                          isPassword: false,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your email';
+                            } else if (!value.contains('@')) {
+                              return 'Please enter a valid email';
+                            }
+                            return null;
+                          },
+                        ),
                         const SizedBox(height: 20),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             _buildLabelText(context, "Password"),
-                            IconButton(
-                              icon: Icon(
-                                color: Color(0xFF838181),
-                                _isPasswordVisible
-                                    ? Icons.visibility_off
-                                    : Icons.visibility,
+                            Row(children: [
+                              IconButton(
+                                icon: Icon(
+                                  color: Color(0xFF838181),
+                                  _isPasswordVisible
+                                      ? Icons.visibility_off
+                                      : Icons.visibility,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _isPasswordVisible = !_isPasswordVisible;
+                                  });
+                                },
                               ),
-                              onPressed: () {
-                                setState(() {
-                                  _isPasswordVisible = !_isPasswordVisible;
-                                });
-                              },
-                            ),
+                              Text(
+                                _isPasswordVisible ? 'Unhide' : 'Hide',
+                                style: TextStyle(
+                                  color: const Color(0xFF838181),
+                                  fontSize: 16,
+                                ),
+                              )
+                            ]),
                           ],
                         ),
                         const SizedBox(height: 2),
@@ -84,7 +117,11 @@ class _SignupState extends State<Signup> {
                               style: ElevatedButton.styleFrom(
                                 backgroundColor: Color(0xFF57CC99),
                               ),
-                              onPressed: () {},
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  // Perform signup action
+                                }
+                              },
                               child: Text(
                                 "Sign Up",
                                 style: TextStyle(
@@ -178,7 +215,14 @@ class _SignupState extends State<Signup> {
                                             GoogleFonts.robotoFlex().fontFamily,
                                       ),
                                       recognizer: TapGestureRecognizer()
-                                        ..onTap = () {},
+                                        ..onTap = () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    HomePage1()),
+                                          );
+                                        },
                                     ),
                                   ],
                                 ),
@@ -200,7 +244,7 @@ class _SignupState extends State<Signup> {
 
   Widget _buildLabelText(BuildContext context, String labelText) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 45.0),
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
       child: Text(
         labelText,
         style: TextStyle(
@@ -212,9 +256,13 @@ class _SignupState extends State<Signup> {
     );
   }
 
-  Widget _buildProjectedTextFormField({required bool isPassword}) {
+  Widget _buildProjectedTextFormField({
+    required TextEditingController controller,
+    required bool isPassword,
+    required String? Function(String?) validator,
+  }) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 45.0),
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
       child: Container(
         decoration: BoxDecoration(
           color: Colors.white,
@@ -228,20 +276,38 @@ class _SignupState extends State<Signup> {
           ],
         ),
         child: TextFormField(
+          controller: controller,
           cursorColor: Color(0xFF838181),
           obscureText: isPassword ? !_isPasswordVisible : false,
           decoration: const InputDecoration(
             border: InputBorder.none,
-            fillColor: Colors.red,
             contentPadding:
                 EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
           ),
+          validator: validator,
         ),
       ),
     );
   }
 
   Widget _buildPasswordField() {
-    return _buildProjectedTextFormField(isPassword: true);
+    return _buildProjectedTextFormField(
+      controller: _passwordController,
+      isPassword: true,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter your password';
+        } else if (!RegExp(r'(?=.*[a-z])').hasMatch(value)) {
+          return 'Password must contain at least one lowercase letter';
+        } else if (!RegExp(r'(?=.*[A-Z])').hasMatch(value)) {
+          return 'Password must contain at least one uppercase letter';
+        } else if (!RegExp(r'(?=.*\d)').hasMatch(value)) {
+          return 'Password must contain at least one digit';
+        } else if (!RegExp(r'(?=.*[!@#\$&*~])').hasMatch(value)) {
+          return 'Password must contain at least one special character';
+        }
+        return null;
+      },
+    );
   }
 }
